b7504483e2e08a23163dc329f87e055527bc9279
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-02-04 14:05:38 -0500

    started from scratch with newproject.sh atreus_astar

--- keyboard/atreus_astar/atreus_astar.c
+++ keyboard/atreus_astar/atreus_astar.c
@@ -27,3 +27,25 @@ void * matrix_scan_kb(void) {
 		(*matrix_scan_user)();
 	}
 };
+
+void bootloader() {
+  clear_keyboard();
+  print("\n\nJump to bootloader... ");
+  _delay_ms(250);
+#ifdef TEENSY
+  cli();
+  UDCON = 1;
+  USBCON = (1<<FRZCLK);  // disable USB
+  UCSR1B = 0;
+  _delay_ms(5);
+  EIMSK = 0; PCICR = 0; SPCR = 0; ACSR = 0; EECR = 0; ADCSRA = 0;
+  TIMSK0 = 0; TIMSK1 = 0; TIMSK3 = 0; TIMSK4 = 0; UCSR1B = 0; TWCR = 0;
+  DDRB = 0; DDRC = 0; DDRD = 0; DDRE = 0; DDRF = 0; TWCR = 0;
+  PORTB = 0; PORTC = 0; PORTD = 0; PORTE = 0; PORTF = 0;
+  asm volatile("jmp 0x7E00");
+#else
+  *(uint16_t *)0x0800 = 0x7777; // these two are a-star-specific
+  bootloader_jump();
+  print("not supported.\n");
+#endif
+}
