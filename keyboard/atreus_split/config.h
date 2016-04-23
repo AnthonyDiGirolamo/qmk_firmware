70abbe1c1e98b40297bddaf1ef62579ca6f271e0
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-04-23 10:02:13 -0700

    update mouse acceleration

--- keyboard/atreus_split/config.h
+++ keyboard/atreus_split/config.h
@@ -80,4 +80,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+#define MOUSEKEY_DELAY 50
+#define MOUSEKEY_INTERVAL 30
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_TIME_TO_MAX 30
+#define MOUSEKEY_WHEEL_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 20
+
 #endif
