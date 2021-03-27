631072942517498e1e86faf25ca9576188b00c3e
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2021-03-27 07:45:44 -0700

    Migrate atreus astar keymap

---
+++ keyboards/atreus/keymaps/anthonydigirolamo/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../../config.h"
+
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_INTERVAL 1
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_TIME_TO_MAX 255
+#define MOUSEKEY_WHEEL_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 255
